52aa6ce2c285bfbf28f3df6f9a9dd6b6dbb24fe7
baredmark583 <baredmark583@gmail.com>
2025-09-25 13:34:36 +0300

    fix(backend): fix build error and improve CORS

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Enable CORS
+  const configService = app.get(ConfigService);
+  const frontendUrl = configService.get<string>('FRONTEND_URL');
+  const adminUrl = configService.get<string>('ADMIN_URL');
+
+  const whitelist = [frontendUrl, adminUrl].filter(Boolean); // Фильтруем пустые значения
+
   app.enableCors({
-    origin: '*', // Be more specific in production
+    origin: (origin, callback) => {
+      // Разрешаем запросы без origin (например, от мобильных приложений или Postman) или если origin в белом списке
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+
 
   // Global Validation Pipe
   app.useGlobalPipes(new ValidationPipe({
