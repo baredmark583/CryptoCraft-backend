ddb667ee5f838b43d432060dc974c8a8863990d7
baredmark583 <baredmark583@gmail.com>
2025-09-27 17:52:16 +0300

    fix(backend): fix build error and improve CORS

--- src/dashboard/dashboard.module.ts
+++ src/dashboard/dashboard.module.ts
@@ -5,9 +5,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Order } from '../orders/entities/order.entity';
 import { User } from '../users/entities/user.entity';
 import { Product } from '../products/entities/product.entity';
+import { Dispute } from '../disputes/entities/dispute.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order, User, Product])],
+  imports: [TypeOrmModule.forFeature([Order, User, Product, Dispute])],
   controllers: [DashboardController],
   providers: [DashboardService],
 })
